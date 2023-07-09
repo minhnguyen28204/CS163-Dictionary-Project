e1d4aec565f30cb4a48b33db1ecfacf2c65e957f
KhoiLeeee <22125042@student.hcmus.edu.vn>
2023-07-09 19:02:51 +0700

    view favorite list
    format file FavoriteList.bin: số từ - size word1 - word1 - size word2 - word2 - ....
    và folder favorite words chứa các file có tên là word.bin (word là các từ trong favoriteList)

---
+++ Header/FavouriteList.cpp
@@ -0,0 +1,25 @@
+#include "FavouriteList.hpp"
+
+vector<string> favouriteList()
+{
+	vector<string> favorite;
+	ifstream fin;
+	fin.open("FavoriteList.bin", ios::binary | ios::in);
+	if (!fin)
+		return favorite;
+	int n;
+	fin.read((char*)&n, sizeof(int));
+	for (int i = 0; i < n; i++)
+	{
+		int size;
+		fin.read((char*)&size, sizeof(int));
+		char* word = new char[size + 1];
+		fin.read((char*)word, size);
+		word[size] = '\0';
+		string output = word;
+		string fileName = "Favorite Words/" + output + ".bin";
+		if (filesystem::exists(fileName))
+			favorite.push_back(output);
+	}
+	return favorite;
+}
--- Header/FavouriteList.hpp
+++ Header/FavouriteList.hpp
@@ -1,7 +1,12 @@
 #ifndef FAVOURITElIST_HPP
 #define FAVOURITELIST_HPP
+#include<vector>
+#include<string>
+#include<fstream>
+#include<filesystem>
+using namespace std;
 
 void addWord();
 void removeWord();
-
+vector<string> favouriteList();
 #endif
