dfd21b36dc287a94328436b0c3e9fe809e954b60
KhoiLeeee <22125042@student.hcmus.edu.vn>
2023-08-04 12:44:02 +0700

    đồng bộ cho giống tên file

--- Header/FavouriteList.cpp
+++ Header/FavouriteList.cpp
@@ -5,7 +5,7 @@ vector<string> favouriteList()
 	vector<string> favorite;
 	ifstream fin;
 	ifstream fin2;
-	fin.open("FavoriteList.bin", ios::binary | ios::in);
+	fin.open("FavoriteList/FavoriteList.bin", ios::binary | ios::in);
 	if (!fin)
 		return favorite;
 	int n;
@@ -18,7 +18,7 @@ vector<string> favouriteList()
 		fin.read((char*)word, size);
 		word[size] = '\0';
 		string output = word;
-		string fileName = "Favorite Words/" + output + ".bin";
+		string fileName = "FavoriteList/" + output + ".bin";
 		fin2.open(fileName);
 		if (fin2)
 		{
@@ -34,7 +34,7 @@ vector<string> favouriteList()
 bool addWordToFavList(std::string& word)
 {
     ifstream fin;
-    std::string path = "\\FavouriteList\\" + word + ".bin";
+    std::string path = "FavouriteList/" + word + ".bin";
     fin.open(path);
     if(fin.is_open()) 
     {
@@ -43,9 +43,19 @@ bool addWordToFavList(std::string& word)
     }
     else fin.close();
     ofstream fout;
-    fout.open("\\FavouriteList\\FavouriteList.bin",ios::binary|ios::app);
+	string fileName = "FavouriteList/FavouriteList.bin";
+	fin.open(fileName);
+	if (!fin)
+		return false;
+	int num;
+	fin.read((char*)&num, sizeof(int));
+	num++;
+	fin.close();
+    fout.open(fileName,ios::binary|ios::app);
     if(!fout.is_open()) return false;
     fout.write((char*)&word,sizeof(word));
+	fout.seekp(0, ios::beg);
+	fout.write((char*)&num, sizeof(int));
     fout.close();
     fout.open(path,ios::binary);
     if(!fout.is_open()) return false;
@@ -55,7 +65,7 @@ bool addWordToFavList(std::string& word)
 
 bool removeWordFromFavList(std::string& word)
 {
-    std::string path = "\\FavouriteList\\" + word + ".bin";
+    std::string path = "FavouriteList/" + word + ".bin";
     if(!remove(path.c_str())) return true;
     return false;
 }
