e62a384e149f59c2f399b53f70f647acac9e32ed
ThisIsNotDat <datqt2407@gmail.com>
2023-08-11 23:42:51 +0700

    Fix bug khi merge

--- Header/Random.hpp
+++ Header/Random.hpp
@@ -7,11 +7,21 @@
 
 namespace Random
 {
-	std::mt19937 generate();
+	std::mt19937 generate()
+	{
+		std::random_device rd{};
+
+		// Create seed_seq with clock and 7 random numbers from std::random_device
+		std::seed_seq ss{
+			static_cast<std::seed_seq::result_type>(std::chrono::steady_clock::now().time_since_epoch().count()),
+				rd(), rd(), rd(), rd(), rd(), rd(), rd() };
+
+		return std::mt19937{ ss };
+	}
 
 	extern std::mt19937 mt;
 
-	int get(int min, int max);
+	//int get(int min, int max);
 
 	template <typename T>
 	T get(T min, T max)
@@ -26,11 +36,11 @@ namespace Random
 	}
 }
 
-std::string getWordFromFile(int& first,int& last);
-int getPositionDef(int& last);
-std::string getDefFromFile(int& pos);
-std::vector<std::string> randomOneWordFourDef();
-std::vector<std::string> randomOneDefFourWord();
+//std::string getWordFromFile(int& first,int& last);
+//int getPositionDef(int& last);
+//std::string getDefFromFile(int& pos);
+//std::vector<std::string> randomOneWordFourDef();
+//std::vector<std::string> randomOneDefFourWord();
 
 #endif //RANDOM_HPP
 
